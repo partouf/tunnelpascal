777d32802eaa5d9c4274eb6eb12381b9c42816cc
florian <florian@freepascal.org>
2021-09-13 22:47:26 +0200

    * fix handling of -0.0 in sse/avx code, resolves #39357

--- tests/webtbs/tw39357.pp
+++ tests/webtbs/tw39357.pp
@@ -6,6 +6,7 @@ function get_sign(d: double): Integer;
   begin
     get_sign:=1;
     p:=pbyte(@d);
+<<<<<<< HEAD
 {$ifdef FPUARM_HAS_FPA}
     inc(p,4);
 {$else}
@@ -14,6 +15,12 @@ function get_sign(d: double): Integer;
 {$endif}
 {$endif}
     if (p^ and $80)<>0 then
+=======
+{$ifdef FPC_LITTLE_ENDIAN}
+    inc(p,4);
+{$endif}
+    if (p^ and $80)=0 then
+>>>>>>> 611164197d...   * fix handling of -0.0 in sse/avx code, resolves #39357
       get_sign:=-1;
   end;
 
@@ -23,10 +30,13 @@ var
     zero : Double;
 begin
     zero:=0.0;
+<<<<<<< HEAD
 
     if get_sign(1.0)<>1 then
       halt(1);
 
+=======
+>>>>>>> 611164197d...   * fix handling of -0.0 in sse/avx code, resolves #39357
 	writeln(-zero);
     if get_sign(-zero)<>-1 then
       halt(1);
