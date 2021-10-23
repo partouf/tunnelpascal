e631baf13d411240e98471cdecb03bb91bb58a73
Dmytro <di@baduk.org>
2021-10-23 14:59:20 +0300

    Added TruncateIntegersOnOverflow compiler option

--- packages/pastojs/src/pas2jsfiler.pp
+++ packages/pastojs/src/pas2jsfiler.pp
@@ -268,7 +268,8 @@ const
     'RTLVersionCheckSystem',
     'RTLVersionCheckUnit',
     'ShortRefGlobals',
-    'ObfuscateLocalIdentifiers'
+    'ObfuscateLocalIdentifiers',
+    'TruncateIntegersOnOverflow'
     );
 
   PCUDefaultTargetPlatform = PlatformBrowser;
