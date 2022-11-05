b5f2e478108df801dfd999574880198b97be3d6f
Ondrej Pokorny <lazarus@kluug.net>
2022-11-05 09:50:18 +0100

    Fix Linux Err code

--- packages/fcl-net/src/ssockets.pp
+++ packages/fcl-net/src/ssockets.pp
@@ -840,7 +840,7 @@ begin
           Dec(Attempts);
         end;
       end;
-  until (IsTimedOutError(Err) and not AAbort and (Result=-1) and (Attempts<>0));
+  until not(IsTimedOutError(Err) and not AAbort and (Result=-1) and (Attempts<>0));
 end;
 
 function TSocketStream.GetLocalAddress: sockets.TSockAddr;
